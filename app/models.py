233ed1c1c903fb87c6f6b8e20b50633c2b048820
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-02-09 13:26:52 +0300

    define login and logout logic

--- app/models.py
+++ app/models.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    '''creates instances of users '''
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -13,8 +17,30 @@ class User(db.Model):
     def __repr__(self):
         return '<user {}>'.format(self.username)
 
+    # generate password hash
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    # verify password
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# tracks of user in user session
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+
+
+
+
+
+
+# remember to add category property
 class Post(db.Model):
+    ''' creates instances of pitched posts '''
+
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     # EAT time specific
@@ -25,4 +51,9 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
     
+
+# remember to add comments
+# class Comment(db.model):
+
+
 
