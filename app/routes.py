f5e57fced2cbb613a31e87ccf533de2d21a0bb98
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-02-09 06:18:24 +0300

    add base template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
 from app import app
+from flask import render_template
+
 
 # A decorator provides mapping between a url and a function.
 # you can chain more than one url to the same fuction.
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+
+    title = 'pitch view'
+    user = {'username': 'loise'}
+    posts = [
+        {
+            'author': {'username': 'Loise'},
+            'body': 'Tis a beautiful day in nairobi'
+        },
+        {
+            'author': {'username': 'June'},
+            'body': 'Some ugali please'
+        }
+    ]
+    return render_template('index.html', user=user, title=title, posts=posts)
