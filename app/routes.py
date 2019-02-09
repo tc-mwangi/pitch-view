2fe48a40da4507496e2cdaa0e3b71f9c965acc92
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-02-09 08:35:49 +0300

    render login form with flash message

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
-from flask import render_template
-
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 # A decorator provides mapping between a url and a function.
 # you can chain more than one url to the same fuction.
@@ -21,3 +21,16 @@ def index():
         }
     ]
     return render_template('index.html', user=user, title=title, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    title = 'Sign In'
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+        
+    return render_template('login.html', title=title, form=form)
