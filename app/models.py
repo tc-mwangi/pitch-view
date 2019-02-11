c7f8a02ebbb01179e8354e6c5a0691dd5bb79b9b
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-11 09:48:23 +0300

    new app structure

--- app/models.py
+++ app/models.py
@@ -1,88 +1,64 @@
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from app import db, login
 from hashlib import md5
+from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import db, login
+
 
 class User(UserMixin, db.Model):
-    '''creates instances of users 
-    
-    Arguments:
-        UserMixin {[type]} -- [description]
-        db {[type]} -- [description]
-    
-    Returns:
-        [type] -- [description]
-    '''
-
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    # relationship link
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+   
 
-    # python method to print class to the console
     def __repr__(self):
-        return '<user {}>'.format(self.username)
+        return '<User {}>'.format(self.username)
 
-    # generate password hash
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    # verify password
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
-# tracks of user in user session
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 
-
-
-# remember to add category property
 class Post(db.Model):
-    '''creates instances of posts
-    
-    Arguments:
-        db {[type]} -- [description]
-    
-    Returns:
-        [type] -- [description]
-    '''
-
-
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    # EAT time specific
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    # establish relationship with user
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # relationship link
-    category = db.relationship('Category', backref='type', lazy='dynamic')
+   
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-    
-
-# remember to add comments
-# class Comment(db.model):
-
-
-class Category(db.Model):
-    ''' '''
-    id = db.Column(db.Integer, primary_key=True)
-    Category = db.Column(db.String(20), index=True, unique=True)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-
-    
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/wanjirumwangi/Desktop/pitch-view/virtual"
+set -gx VIRTUAL_ENV "/Users/wanjirumwangitop_cat/Desktop/pitchview/pitch-view/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
