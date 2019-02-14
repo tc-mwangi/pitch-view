9065862d4b583ce812139e206321027c288b7cd6
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-14 14:23:38 +0300

    test deploy 3

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
-from app import app, db
+from app import create_app , db 
 from app.models import User, Post
+
+app = create_app()
+
 
 # starts a python interpretor within the app, helps in testing
 @app.shell_context_processor
